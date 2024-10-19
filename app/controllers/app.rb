e48a57ab884b6eb30fb998645f664e853c7a61cf
EiKyiPhyuKhin <eikyiphyukhin007@gmail.com>
2024-10-20 04:04:24 +0800

    fix dir, reek, rubocop, flog

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -7,26 +7,33 @@ require_relative '../models/gateways/openai_api'
 require_relative '../models/mappers/dish_mapper'
 
 module MealDecoder
+  # The `App` class is the main application for the MealDecoder web service.
+  # It handles routing, renders views, and integrates with external APIs to fetch dish information.
+  # This class uses the Roda framework for routing and Slim for templating.
   class App < Roda
     plugin :environments
     plugin :render, engine: 'slim', views: 'app/view'
 
-    # Correctly serve static files from the 'view/assets' folder
+    # Serve static files from the 'view/assets' folder
     plugin :public, root: File.join(__dir__, '../view/assets')
     plugin :static, ['/assets']
 
-    route do |r|
-      r.public # Serve static files like CSS and images
+    route do |request|
+      request.public # Serve static files like CSS and images
 
-      r.root do
+      request.root do
         view 'index', locals: { error: nil }
       end
 
-      r.post 'fetch_dish' do
-        dish_name = r.params['dish_name'].strip
+      request.post 'fetch_dish' do
+        dish_name = request.params['dish_name'].strip
+
         if dish_name.match?(/\A[\p{L}\s]+\z/u)
           api_key = MealDecoder::Configuration::OPENAI_API_KEY
-          dish = MealDecoder::Mappers::DishMapper.new(MealDecoder::Gateways::OpenAIAPI.new(api_key)).find(dish_name)
+          dish = MealDecoder::Mappers::DishMapper.new(
+            MealDecoder::Gateways::OpenAIAPI.new(api_key)
+          ).find(dish_name)
+
           view 'display_dish', locals: { dish: }
         else
           view 'index', locals: { error: 'Invalid dish name. Please enter a valid name (letters and spaces only).' }
