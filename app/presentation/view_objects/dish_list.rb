a643caa88f6d2e0945a551e62f790e342169f9bf
Ken Kanokphan <kp.thananchai@gmail.com>
2024-12-19 05:25:25 +0800

    remove unused code (comment)

--- app/presentation/view_objects/dish_list.rb
+++ app/presentation/view_objects/dish_list.rb
@@ -1,34 +1,3 @@
-# module MealDecoder
-#   module Views
-#     # View object for rendering a collection of dishes
-#     # Handles iteration and counting of dish entities
-#     class DishesList
-#       def initialize(dishes)
-#         puts "DishesList initializing with #{dishes.length} dishes"
-#         @dishes = dishes.map do |dish|
-#           puts "Creating view for dish: #{dish.name}"
-#           Dish.new(dish)
-#         end
-#       end
-
-#       def any?
-#         result = @dishes.any?
-#         puts "DishesList#any? returning: #{result}"
-#         result
-#       end
-
-#       def each(&block)
-#         @dishes.each(&block)
-#       end
-
-#       def count
-#         @dishes.count
-#       end
-#     end
-#   end
-# end
-
-# app/presentation/view_objects/dish_list.rb
 # frozen_string_literal: true
 
 module MealDecoder
